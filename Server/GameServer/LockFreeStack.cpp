1acda057025dfb9790ca2d634ba82da716e4f754
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2021-09-23 22:17:58 +0900

    Memory Pool #2

--- Server/GameServer/LockFreeStack.cpp
+++ Server/GameServer/LockFreeStack.cpp
@@ -33,7 +33,7 @@ SListEntry* PopEntrySList(SListHeader* header)
 /*----------------
 	  2차 시도
 ----------------*/
-
+/*
 void InitializeHead(SListHeader* header)
 {
 	header->next = nullptr;
@@ -65,4 +65,61 @@ SListEntry* PopEntrySList(SListHeader* header)
 	}
 
 	return expected;
+}
+*/
+
+/*----------------
+	  3차 시도
+----------------*/
+
+void InitializeHead(SListHeader* header)
+{
+	header->alignment = 0;
+	header->region = 0;
+}
+
+void PushEntrySList(SListHeader* header, SListEntry* entry)
+{
+	SListHeader expected = {};
+	SListHeader desired = {};
+	
+	// 16 바이트 정렬
+	desired.HeaderX64.next = (((uint64)entry) >> 4);
+
+	while (true)
+	{
+		expected = *header;
+		// 이 사이에 변경될 수 있다
+		entry->next = (SListEntry*)(((uint64)expected.HeaderX64.next) << 4);
+		desired.HeaderX64.depth = expected.HeaderX64.depth + 1;
+		desired.HeaderX64.sequence = expected.HeaderX64.sequence + 1;
+
+		if (::InterlockedCompareExchange128((int64*)header, desired.region, desired.alignment, (int64*)&expected) == 1)
+			break;
+	}
+}
+
+SListEntry* PopEntrySList(SListHeader* header)
+{
+	SListHeader expected = {};
+	SListHeader desired = {};
+	SListEntry* entry = nullptr;
+
+	while (true)
+	{
+		expected = *header;
+		entry = (SListEntry*)(((uint64)expected.HeaderX64.next) << 4);
+		if (entry == nullptr)
+			break;
+
+		// Use-After-Free
+		desired.HeaderX64.next = ((uint64)entry->next) >> 4;
+		desired.HeaderX64.depth = expected.HeaderX64.depth - 1;
+		desired.HeaderX64.sequence = expected.HeaderX64.sequence + 1;
+
+		if (::InterlockedCompareExchange128((int64*)header, desired.region, desired.alignment, (int64*)&expected) == 1)
+			break;
+	}
+
+	return entry;
 }
--- Server/GameServer/LockFreeStack.h
+++ Server/GameServer/LockFreeStack.h
@@ -24,18 +24,57 @@ SListEntry* PopEntrySList(SListHeader* header);
 	  2차 시도
 ----------------*/
 
+//struct SListEntry
+//{
+//	SListEntry* next;
+//};
+//
+//struct SListHeader
+//{
+//	SListEntry* next = nullptr;
+//};
+//
+//// [data][  ][  ]
+//// Header[next]
+//void InitializeHead(SListHeader* header);
+//void PushEntrySList(SListHeader* header, SListEntry* entry);
+//SListEntry* PopEntrySList(SListHeader* header);
+
+/*----------------
+	  3차 시도
+----------------*/
+
+DECLSPEC_ALIGN(16)
 struct SListEntry
 {
 	SListEntry* next;
 };
 
+DECLSPEC_ALIGN(16)
 struct SListHeader
 {
-	SListEntry* next = nullptr;
+	SListHeader()
+	{
+		alignment = 0;
+		region = 0;
+	}
+	union
+	{
+		struct
+		{
+			uint64 alignment;
+			uint64 region;
+		} DUMMYSTRUCTNAME;
+		struct
+		{
+			uint64 depth : 16;
+			uint64 sequence : 48;
+			uint64 reserved : 4;
+			uint64 next : 60;
+		} HeaderX64;
+	};
 };
 
-// [data][  ][  ]
-// Header[next]
 void InitializeHead(SListHeader* header);
 void PushEntrySList(SListHeader* header, SListEntry* entry);
 SListEntry* PopEntrySList(SListHeader* header);
