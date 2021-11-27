c3a08eb875f29de924544f8ac12a5658e560fc8b
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2021-11-27 17:48:27 +0900

    네트워크 라이브러리 만들기(SocketUtils)

--- Server/ServerCore/CorePch.h
+++ Server/ServerCore/CorePch.h
@@ -10,7 +10,12 @@
 #include <iostream>
 using namespace std;
 
-#include "Lock.h"
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
+#include "Lock.h"
 #include "ObjectPool.h"
 #include "TypeCast.h"
+#include "Memory.h"
