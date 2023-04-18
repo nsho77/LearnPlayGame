2582159b6377c0a7daa9e47009911f395bf88c9e
shroh <nsho77@naver.com>
2023-04-18 13:08:23 +0900

    feat: Packet Session

--- Server/ServerCore/SendBuffer.cpp
+++ Server/ServerCore/SendBuffer.cpp
@@ -89,6 +89,8 @@ SendBufferRef SendBufferManager::Open(uint32 size)
 
 SendBufferChunkRef SendBufferManager::Pop()
 {
+	cout << "Pop SENDBUFFERCHUNK" << endl;
+
 	{
 		WRITE_LOCK;
 		if (_sendBufferChunks.empty() == false)
@@ -110,5 +112,7 @@ void SendBufferManager::Push(SendBufferChunkRef buffer)
 
 void SendBufferManager::PushGlobal(SendBufferChunk* buffer)
 {
+	cout << "PushGlobal SENDBUFFERCHUNK" << endl;
+
 	GSendBufferManager->Push(SendBufferChunkRef(buffer, PushGlobal));
 }
